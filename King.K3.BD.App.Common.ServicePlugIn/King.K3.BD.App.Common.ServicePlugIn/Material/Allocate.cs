30eb8e424f2162323f0a1c26fb1ef82a747e0561
zh l <1811226078@qq.com>
2023-12-23 10:20:36 +0800

    物料分配

---
+++ King.K3.BD.App.Common.ServicePlugIn/King.K3.BD.App.Common.ServicePlugIn/Material/Allocate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.Util;
+
+namespace King.K3.BD.App.Common.ServicePlugIn.Material
+{
+	// Token: 0x02000007 RID: 7
+	[Description("物料分配操作自动更新供应来源")]
+	[HotUpdate]
+	public class Allocate : AbstractOperationServicePlugIn
+	{
+		// Token: 0x0600001B RID: 27 RVA: 0x00003DEC File Offset: 0x00001FEC
+		public override void AfterExecuteOperationTransaction(AfterExecuteOperationTransaction e)
+		{
+			foreach (DynamicObject dynamicObject in e.DataEntitys)
+			{
+				base.AfterExecuteOperationTransaction(e);
+				string str = Convert.ToString(dynamicObject["Number"]);
+				string text = "/*dialect*/\r\nUPDATE T_BD_MATERIALPLAN SET  FSUPPLYSOURCEID = \r\nCASE \r\nWHEN FUSEORGID = 1023812 THEN  1194398  \r\nWHEN FUSEORGID = 1023805 THEN  1194394 END  \r\nFROM T_BD_MATERIALPLAN as mlp\r\nINNER JOIN T_BD_MATERIAL AS ml ON ml.FMATERIALID = mlp.FMATERIALID\r\nINNER JOIN T_BD_MATERIALBASE AS mlb ON mlb.FMATERIALID = ml.FMATERIALID\r\nWHERE ml.FNUMBER = '" + str + "'  AND FERPCLSID = 1";
+				DBUtils.Execute(base.Context, text);
+			}
+		}
+	}
+}
---
+++ King.K3.BD.App.Common.ServicePlugIn/King.K3.BD.App.Common.ServicePlugIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("King.K3.BD.App.Common.ServicePlugIn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.K3.BD.App.Common.ServicePlugIn")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("db0ccbcf-e074-440a-a26b-2eb904205978")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
