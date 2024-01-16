7b18a099e6e08aafa6ebe01650f6e7af0aaeb57b
zh l <1811226078@qq.com>
2024-01-16 14:33:22 +0800

    WIP报表

---
+++ WIPRPT/TOKEN.WIPRPT/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("TOKEN.WIPRPT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TOKEN.WIPRPT")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("71925c7d-0483-4a1e-be8a-c11a34b64e9e")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
